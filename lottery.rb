51218922a0580431e28227c5c3e4fe21e07b5841
Paul Curry <pcurry@gmail.com>
2011-04-01 21:49:14 -0400

    Added in the AnimalLottoTicket class.

--- lottery.rb
+++ lottery.rb
@@ -59,5 +59,44 @@ class LotteryDraw
 end
 
 
+class AnimalLottoTicket
+  
+  # A list of valid notes.
+  NOTES = [ :Ab, :A, :Bb, :B, :C, :Db, :D, :Eb, :E, :Gb, :G ]
+  
+  # Stores the three picked notes and a purchase date.
+  attr_reader :picks, :purchased
+
+  # Creates a new ticket from three chosen notes.  The three notes
+  # must be unique notes.
+  def initialize(note1, note2, note3)
+    picks = [note1, note2, note3]
+    if picks.uniq!
+      raise ArgumentError, "The three notes picked must be different notes."
+    elsif picks.detect { |p| not NOTES.include? p }
+      raise ArgumentError, "The three picks must be notes in a chromatic scale."
+    end
+    @picks = picks
+    @purchased = Time.now
+  end
+
+  # Score this ticket against the final draw
+  def score(final)
+    count = 0
+    final.picks.each do |note|
+      count +=1 if picks.include? note
+    end
+    count
+  end
+  
+  # Constructor to create a random AnimalLottoTicket.
+  def self.new_random
+    new( NOTES[ rand( NOTES.length) ], NOTES[ rand( NOTES.length) ], 
+         NOTES[ rand( NOTES.length) ] )
+  rescue ArgumentError
+    redo
+  end
+
+end
 
         
